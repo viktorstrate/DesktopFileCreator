45f2c1a79ffea746828e5d0f10462621f464d899
viktorstrate <viktorstrate@gmail.com>
2015-03-09 18:02:25 +0100

    Made the GUI

--- src/dk/qpqp/Main.java
+++ src/dk/qpqp/Main.java
@@ -5,6 +5,6 @@ package dk.qpqp;
  */
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ src/dk/qpqp/MainWindow.java
@@ -0,0 +1,24 @@
+package dk.qpqp;
+
+import javax.swing.*;
+
+/**
+ * Created by viktorstrate on 3/9/15.
+ */
+public class MainWindow {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton browseButton;
+    private JTextField textField4;
+    private JButton browseButton1;
+    private JList list1;
+    private JTextField textField5;
+    private JComboBox comboBox1;
+    private JTabbedPane tabbedPane1;
+    private JTextField textField6;
+    private JButton browseButton2;
+    private JButton generateButton;
+    private JTextField applicationTextField;
+    private JTextField a10TextField;
+}
