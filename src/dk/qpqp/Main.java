b2fbf9447f082c0f53bee33c074b9a45367a6828
viktorstrate <viktorstrate@gmail.com>
2015-03-09 18:47:58 +0100

    Made the window show and changed the Look and Feel to system standard

--- src/dk/qpqp/Main.java
+++ src/dk/qpqp/Main.java
@@ -5,6 +5,7 @@ package dk.qpqp;
  */
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello World");
+        //MainWindow window = new MainWindow();
     }
 }
--- src/dk/qpqp/MainWindow.java
+++ src/dk/qpqp/MainWindow.java
@@ -1,11 +1,12 @@
 package dk.qpqp;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by viktorstrate on 3/9/15.
  */
-public class MainWindow {
+public class MainWindow extends JFrame {
     private JTextField txtName;
     private JTextField txtDesc;
     private JTextField txtIcon;
@@ -34,4 +35,11 @@ public class MainWindow {
     private JLabel labelType;
     private JLabel labelVersion;
     private JLabel labelOutput;
+
+    public MainWindow() throws HeadlessException {
+        super("Desktop File Creator");
+        pack();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
